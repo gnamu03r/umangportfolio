30b099009af03d67514cc1e2a648741e589d7ada
umang <umang03r@gmail.com>
2025-01-01 12:01:18 +0530

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <div className="home__social">
+            <a href="https://facebook.com" className="home__social-icon" target="_blank"><i className="uil uil-facebook-f"></i></a>
+            <a href="https://github.com" className="home__social-icon" target="_blank"><i className="uil uil-github-alt"></i></a>
+            <a href="https://instagram.com" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
+          </div>
+
+          <div className="home__img">
+            <svg className="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <mask id="mask0" mask-type="alpha">
+                 
+    </section>
+  )
+}
+
+export default Home
