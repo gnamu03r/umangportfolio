15bec0ad92d76896240526aec9730ab97a4cf06f
umang <umang03r@gmail.com>
2025-01-01 12:06:35 +0530

    second commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,11 +10,8 @@ const Home = () => {
             <a href="https://github.com" className="home__social-icon" target="_blank"><i className="uil uil-github-alt"></i></a>
             <a href="https://instagram.com" className="home__social-icon" target="_blank"><i className="uil uil-instagram"></i></a>
           </div>
-
-          <div className="home__img">
-            <svg className="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-              <mask id="mask0" mask-type="alpha">
-                 
+        </div>
+      </div>
     </section>
   )
 }
